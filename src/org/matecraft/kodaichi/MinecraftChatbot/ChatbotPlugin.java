f3addc497af3f7406b42eb1277c04d4c7222839b
KodaichiZero <evil_berries43@hotmail.com>
2015-11-27 03:37:47 -0500

    Project files and basic setup

---
+++ src/org/matecraft/kodaichi/MinecraftChatbot/ChatbotPlugin.java
@@ -0,0 +1,11 @@
+package org.matecraft.kodaichi.MinecraftChatbot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChatbotPlugin extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+}
